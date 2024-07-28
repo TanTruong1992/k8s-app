c53b4bd54c5e52ae4b2190eabc1a74ec16d7d09d
TanTruong1992 <nguyenvantruong.ktx@gmail.com>
2024-07-28 14:20:20 +0700

    first commit

---
+++ main-ec2.tf
@@ -0,0 +1,27 @@
+
+provider "aws" {
+  region = "ap-southeast-1"
+}
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+
+  owners = ["08888888888"]
+}
+
+resource "aws_instance" "ec2-terraform" {
+  count = 5
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t2.micro"
+}
+
+output "ec2" {
+  value = {
+    public_ip = [ for v in aws_instance.ec2-terraform : v.public_ip ]
+  }
+}
